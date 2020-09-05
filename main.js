4ca43cb05f3fd400e8165a95a1fe856354b36d40
FN <70086013+fnbots-wa@users.noreply.github.com>
2020-09-05 11:35:40 +0700

    Upload main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+const { create, decryptMedia } = require('@open-wa/wa-automate')
+const { NotificationLanguage } = require('@open-wa/wa-automate/dist/api/model')
+const confObj = {
+    authTimeout: 0,
+    autoRefresh: true,
+    blockCrashLogs: true,
+    cacheEnabled: false,
+    disableSpins: true,
+    executablePath:'/usr/bin/google-chrome-stable',
+    hostNotificationLang: NotificationLanguage.IDID,
+    headless: true,
+    killProcessOnBrowserClose: true,
+    qrRefreshS: 20,
+    qrTimeout: 0,
+    safeMode: true
+    useChrome: true
+}
+const runSession = async() => {
+    create('fnbots', confObj)
+        .then((client) => {
+            client.onStateChanged((state) => {
+                if (state === 'CONFLICT') client.forceRefocus()
+            })
+            client.onMessage((message) => {
+                msgHandler(client, message)
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+runSession()

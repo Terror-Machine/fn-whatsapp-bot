8f4cd0cfd4e2b8e12c920e386d63247f75ee7adc
FN <70086013+Terror-Machine@users.noreply.github.com>
2020-10-19 10:18:18 +0700

    Create fn.sh

---
+++ fn.sh
@@ -0,0 +1,24 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install curl -y
+sudo apt install unzip -y
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+sudo apt-get install gcc g++ make -y
+sudo apt install python3-pip -y
+sudo apt install git -y
+apt install python3-pafy -y
+sudo apt install ffmpeg -y
+sudo apt install imagemagick -y
+sudo apt install gifsicle -y
+sudo apt install tesseract-ocr -y
+sudo apt install libtesseract-dev -y
+sudo apt-get update && sudo apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+sudo timedatectl set-timezone Asia/Jakarta
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+pip3 install pydub
+pip3 install nhentai
+pip3 install youtube-dl
+pip3 install img2pdf

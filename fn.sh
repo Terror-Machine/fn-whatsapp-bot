86c57321f787f860a516801119464c0b8d49810b
FN <70086013+Terror-Machine@users.noreply.github.com>
2020-10-28 18:54:18 +0700

    Update fn.sh

--- fn.sh
+++ fn.sh
@@ -21,8 +21,8 @@ sudo apt-get install -y libicu-dev libpango1.0-dev libcairo2-dev
 sudo apt-get update && sudo apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 sudo timedatectl set-timezone Asia/Jakarta
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-git clone https://github.com/Terror-Machine/fnbots
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+git clone https://github.com/Terror-Machine/fn-whatsapp-bot
 cd fnbots
 pip3 install ffmpeg
 pip3 install pydub

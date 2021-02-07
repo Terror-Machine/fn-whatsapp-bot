da8530b82a751010657519f2f2762da6647d8d5d
FN <70086013+Terror-Machine@users.noreply.github.com>
2021-02-08 02:32:22 +0700

    Update fn.sh

--- fn.sh
+++ fn.sh
@@ -23,9 +23,9 @@ apt-get install -y libjpeg-dev libpng-dev libtiff-dev libgif-dev
 apt-get install -y libicu-dev libpango1.0-dev libcairo2-dev
 apt-get update && apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 timedatectl set-timezone Asia/Jakarta
-npm install pm2 -g
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
+apt-get install -y speedtest-cli
 pip3 install ffmpeg
 pip3 install pydub
 pip3 install youtube-dl
@@ -39,4 +39,3 @@ npm i -g cwebp
 npm i -g npm
 npm i -g pm2
 npm i
-npm start
